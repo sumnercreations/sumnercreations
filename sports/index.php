37d10a8ed5b3ebf0435eba9639a6d379147ac644
Ammon Lockwood <ammon@sumnercreations.com>
2012-02-08 21:12:36 -0700

    - renamed the index file to .php instead of .html so that I could include a header and navigation file

---
+++ sports/index.php
@@ -0,0 +1,29 @@
+<html>
+<?php include("includes/header.inc"); ?>
+<body>
+	<?php include("includes/navigation.inc"); ?>
+	<div class="container content">
+		<div class="well">
+			<h3>Welcome to the Magna Stake Sports Page!</h3>
+			<p>Use the links above to find the sport or event that you are looking for.</p>
+		</div><!-- /well -->
+		<div class="well">
+			<h3 class="center-text">Upcoming Events</h3>
+			<div class="row">
+			<div class="span12">
+				<div class="row">
+					<div class="span6">
+						<h2 class="center-text">Men</h2><br />
+						<iframe src="https://www.google.com/calendar/b/0/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=400&amp;wkst=1&amp;bgcolor=%23ffffff&amp;src=drhnnoptr3hdfshl774qmumjhs%40group.calendar.google.com&amp;color=%232F6309&amp;ctz=America%2FDenver" style=" border-width:0 " width="420" height="400" frameborder="0" scrolling="no"></iframe>
+					</div>
+					<div class="span6">
+						<h2 class="center-text">Young Men</h2><br />
+						<iframe src="https://www.google.com/calendar/b/0/embed?showTitle=0&amp;showNav=0&amp;showDate=0&amp;showCalendars=0&amp;showTz=0&amp;mode=AGENDA&amp;height=400&amp;wkst=1&amp;bgcolor=%23FFFFFF&amp;src=dpq5mq80i5220kltvfkn1mqhcc%40group.calendar.google.com&amp;color=%23875509&amp;ctz=America%2FDenver" style=" border-width:0 " width="420" height="400" frameborder="0" scrolling="no"></iframe>
+					</div>
+				</div>
+			</div>
+		</div>
+		</div>
+	</div><!-- /content -->
+</body>
+</html>
