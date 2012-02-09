df5dd6fa6f98f79d2a68c2ce791e42ebee3ec199
Ammon Lockwood <ammon@sumnercreations.com>
2012-02-08 21:14:02 -0700

    - added the sports pages so far.  they are all the same and simply show an under construction image. - added the styles.css file

---
+++ sports/football.php
@@ -0,0 +1,14 @@
+<html>
+<?php include("includes/header.inc"); ?>
+<body>
+	<?php include("includes/navigation.inc"); ?>
+	<div class="container content">
+		<div class="well">
+			<h1 class="center-text">Flag Football</h1>
+		</div><!-- /well -->
+		<div class="well">
+			<img src="http://officecubicledotorg.files.wordpress.com/2011/03/website-under-construction.gif" alt="Under Construction" />
+		</div><!-- /well -->
+	</div><!-- /container content -->
+</body>
+</html>
